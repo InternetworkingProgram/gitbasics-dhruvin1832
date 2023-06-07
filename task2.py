bc59a46d766793fa702fc31efc6e1baa3353671c
dhruvin1832 <53215822+dhruvin1832@users.noreply.github.com>
2023-06-06 22:33:40 -0300

    Lab 1 files

---
+++ task2.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=40)
+
+logging.error('This is an error message')
+
+logging.critical('This is a critical message')
+
+logging.debug('This is a debug message')
+
+logging.info('This is an info message')
+
+logging.warning('This is a warning message')
